9adfd71f1dd1951e047d5efb926a54bdd28042c0
ganeshpadole <ganeshpadole55@gmail.com>
2023-07-17 11:18:12 +0530

    - added constants

---
+++ FlickrCodeChallenge/Constants/FlickrConstants.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrConstants.swift
+//  FlickrCodeChallenge
+//
+//  Created by Ganesh on 17/07/23.
+//
+
+import Foundation
+
+struct FlickrConstants {
+    static let apiKey = "a724969f016f0b8badd7e518a6c48e55"
+    static let method = "flickr.photos.search"
+    static let extras = "url_t"
+}
